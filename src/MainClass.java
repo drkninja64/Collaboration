4f945095e1fa099f28fbaef7c1a84a49da7cb4be
Sajag Acharya <sajag.acharya@gmail.com>
2014-11-07 16:28:23 +0545

    Said Hello

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,5 +1,8 @@
 /**
- * Created by Admin on 11/7/2014.
+ * Created by Sajag on 11/7/2014.
  */
 public class MainClass {
+    public static void main(String[] args){
+        System.out.println("Hello world");
+    }
 }
