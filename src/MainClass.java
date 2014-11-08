1bb8d4744d06a99c1f0b73513616de9302c1bf5b
russellsubedi <russell.subedi@gmail.com>
2014-11-08 14:17:32 +0545

    Message Displayer

--- src/MainClass.java
+++ src/MainClass.java
@@ -10,11 +10,16 @@ public class MainClass {
         String IpMessage;
         IpMessage = ipMsg();
         opMsg(IpMessage);
+
+        /* added for factorial purposes */
+        System.out.print("\nEnter an integer: ");
+        int Int = read.nextInt();
+        System.out.println("Factorial = " + Factorial(Int));
     }
 
     /**
      * Asks user to enter a message
-     * @return The inputted message
+     * @return The inputted message -> "inputted" ??
      */
     private static String ipMsg(){
         System.out.print("Please enter your message: ");
@@ -28,5 +33,22 @@ public class MainClass {
     private static void opMsg(String Message){
         System.out.println("Your message was:");
         System.out.println(Message);
+    }
+
+    /**
+     * @param Integer is the number whose factorial is to be calculated
+     * It's recursive if factorial is out of range or infinite
+     */
+    private static double Factorial(int Integer)
+    {
+        double Fact = 1;
+        if (Integer < 0 || Integer > 171)   // factorial of a negative number is infinite
+            return Factorial(171);  // factorial of Integer >= 171 is out of range, i.e. Infinity
+        else
+        {
+            for (int i = 2; i <= Integer; ++i)
+                Fact *= i;
+            return Fact;
+        }
     }
 }
