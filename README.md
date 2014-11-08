f5cee8f21d254fe128b23e1989d6c0ee3b38ba62
Sajag Acharya <sajag.acharya@gmail.com>
2014-11-08 20:33:28 +0545

    Did cool stuff

--- README.md
+++ README.md
@@ -3,4 +3,13 @@ Collaboration
 
 Testing with Russell
 
+Messages
+========
+
 * Press Ctrl + Q over a function to view its Javadoc
+* Always use this to update the other person about changes, and if viewed, change * to -, or delete it like the above one, once you view it
+
+Changelog
+=========
+
+* Used separate class for factorial and message programs
--- src/MainClass.java
+++ src/MainClass.java
@@ -1,54 +1,11 @@
-import java.util.Scanner;
 
 /**
  * Created by Sajag on 11/7/2014.
  */
 public class MainClass {
 
-    static Scanner read = new Scanner(System.in);
     public static void main(String[] args){
-        String IpMessage;
-        IpMessage = ipMsg();
-        opMsg(IpMessage);
-
-        /* added for factorial purposes */
-        System.out.print("\nEnter an integer: ");
-        int Int = read.nextInt();
-        System.out.println("Factorial = " + Factorial(Int));
+        new Factorial();
     }
 
-    /**
-     * Asks user to enter a message
-     * @return The inputted message -> "inputted" ??
-     */
-    private static String ipMsg(){
-        System.out.print("Please enter your message: ");
-        return read.nextLine();
-    }
-
-    /**
-     * Displays the message with obviousness
-     * @param Message the message to be displayed
-     */
-    private static void opMsg(String Message){
-        System.out.print("Your message was: ");
-        System.out.println(Message);
-    }
-
-    /**
-     * @param Integer is the number whose factorial is to be calculated
-     * It's recursive if factorial is out of range or infinite
-     */
-    private static double Factorial(int Integer)
-    {
-        double Fact = 1;
-        if (Integer < 0 || Integer > 171)   // factorial of a negative number is infinite
-            return Factorial(171);  // factorial of Integer >= 171 is out of range, i.e. Infinity
-        else
-        {
-            for (int i = 2; i <= Integer; ++i)
-                Fact *= i;
-            return Fact;
-        }
-    }
 }
